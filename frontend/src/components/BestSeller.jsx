4910f675230f0e128f9599297d2726b1b79e29df
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-03-09 13:17:51 +0530

    upadted bestseller part

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -8,9 +8,9 @@ const BestSeller = () => {
   const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
-    const bestProduct = products.filter((item) => (item.bestSeller));
-    setBestSeller(bestProduct.slice(0,5))
-  }, []);
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
+  }, [products]);
 
   return (
     <div className='my-10'>
@@ -21,11 +21,9 @@ const BestSeller = () => {
         </p>
       </div>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6'>
-        {
-            bestSeller.map((item, index) => (
-                <ProductItem key={index} id={item.id} name={item.name} image={item.image}  price={item.price} />
-        ))
-        }
+        {bestSeller.map((item, index) => (
+          <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+        ))}
       </div>
     </div>
   )
