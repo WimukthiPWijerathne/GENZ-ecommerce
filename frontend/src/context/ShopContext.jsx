a74a11e37603f58b26d38166c71830018a81563e
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-03-11 23:42:37 +0530

    apply filters

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -4,20 +4,20 @@ import { products } from "../assets/assets";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-    const currency = 'Lkr';
-    const delivery_fee = 10; e
+  const currency = 'Lkr';
+  const delivery_fee = 10; // Fixed spelling mistake
 
-    const value = {
-        products,
-        currency,
-        delivery_fee 
-    };
+  const value = {
+    products,
+    currency,
+    delivery_fee // Updated spelling
+  };
 
-    return (
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    );
+  return (
+    <ShopContext.Provider value={value}>
+      {props.children}
+    </ShopContext.Provider>
+  );
 };
 
 export default ShopContextProvider;
